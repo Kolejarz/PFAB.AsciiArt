48cf6810bad04d334c7ff9ac9abf54ca7950c0e7
Tomasz Kras <tkras@future-processing.com>
2021-11-17 00:33:29 +0100

    Initial project with file selection

---
+++ PFAB.AsciiArt.Runner/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using Utility.CommandLine;
+
+namespace PFAB.AsciiArt.Runner
+{
+    class Program
+    {
+        [Argument('p', "path", "Path to the image that would be processed to ASCII")]
+        private static string Path { get; set; }
+
+        static void Main(string[] args)
+        {
+            Arguments.Populate();
+
+            using var file = File.Open(Path, FileMode.Open);
+            Console.WriteLine(file.Length);
+        }
+    }
+}
